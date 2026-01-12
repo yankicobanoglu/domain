a228d8eff8b04fd3d597932b5512b32af016473e
yankicobanoglu <yanki_cobanoglu@hotmail.com>
2026-01-12 12:22:59 +0100

    Delete types.ts

--- types.ts
+++
@@ -1,8 +0,0 @@
-export interface Game {
-  id: string;
-  title: string;
-  description: string;
-  url: string; // The URL to your Vercel project
-  thumbnail: string; // Image URL for the card
-  category: string;
-}
